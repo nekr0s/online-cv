9094216ae9b1901890449efe174bb6d33a7194d2
nekr0s <alexanderrusev23@gmail.com>
2022-10-09 18:11:55 -0400

    Add codenames for projects

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,21 +1,17 @@
-jQuery(document).ready(function($) {
-
+jQuery(document).ready(function ($) {
     $('.level-bar-inner').css('width', '0');
-    
-    $(window).on('load', function() {
 
-        $('.level-bar-inner').each(function() {
-        
+    $(window).on('load', function () {
+        $(function () {
+            $('[data-toggle="tooltip"]').tooltip()
+        });
+
+        $('.level-bar-inner').each(function () {
             var itemWidth = $(this).data('level');
-            
+
             $(this).animate({
                 width: itemWidth
             }, 800);
-            
         });
-
     });
-   
-    
-
 });
